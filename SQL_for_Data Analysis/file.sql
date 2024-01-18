bf73ffb475c5b2177a687feea5bd4087b45044ea
shamreen20 <shamreenstudy1@gmail.com>
2024-01-18 11:04:41 +0100

    add SQL folder

---
+++ SQL_for_Data Analysis/file.sql
@@ -0,0 +1,11 @@
+ --> Aliasing a column name in SQL:
+
+SELECT 
+       [Organization Level] = [OrganizationLevel]
+      ,[JobTitle]
+      ,[HireDate]
+      ,[VacationHours]
+	  
+FROM [AdventureWorks2019].[HumanResources].[Employee]
+
+--> 
